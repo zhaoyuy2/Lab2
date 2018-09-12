18d2ca8b810dc0fe7b6f04ae8efef9b4692f9eca
zhaoyuy2 <zhaoyuy2@illinois.edu>
2018-09-12 16:11:59 -0500

    1

--- src/main/java/SolveMaze.java
+++ src/main/java/SolveMaze.java
@@ -35,6 +35,13 @@ public class SolveMaze {
          */
         for (int step = 0; step < 1000; step++) {
             // Implement your maze solving algorithm here
+            while (maze.isFinished() == false) {
+                maze.turnLeft();
+                while (maze.canMove() == false) {
+                    maze.turnRight();
+                }
+            }
+            maze.move();
         }
 
         if (maze.isFinished()) {
